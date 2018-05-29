79d861afdc2e2a0fb05f310de0bcae0b08bec08c
onaug6th <592986151@qq.com>
2018-05-29 11:19:10 +0800

    modify environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,9 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-
+//  server中填写本地启动的后端服务端口哦
 export const environment = {
   production: false,
-  // server:"http://localhost:3000/",
-  server:"http://www.onaug6th.com/",
+  server: "http://localhost:3000/",
   timeout: 60 * 1000,
 };
